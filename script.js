c2139bace672347a18abb0bccf9b46d0ad8dcb15
Daniel-Centeno <daniel100teno@gmail.com>
2025-01-06 18:39:17 -0300

    add task function

--- script.js
+++ script.js
@@ -2,5 +2,15 @@ const input = document.getElementById('todo-input');
 const todoList = document.getElementById('todo-list');
 
 function addTask(){
-    
+    const li = document.createElement('li');
+    li.textContent = task;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'X';
+    deleteButton.addEventListener('click', function() {
+        todoList.removeChild(li);
+    });
+
+    li.appendChild(deleteButton);
+    todoList.appendChild(li);
 }
